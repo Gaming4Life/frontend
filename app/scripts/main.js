d2562bde0444ad14ef138d88f8c11dd271286078
Marcus Holgersson <marcus.holgersson@softhouse.se>
2014-09-24 15:05:45 +0200

    fixed so correct group is shown

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,12 +17,25 @@ g4lApp.controller('ViewGroupController', ['$scope', '$http', function ($scope, $
   $http.get('fixtures/challanges.json').success(function(data) {
   	$scope.challanges = data;
   });
+$http.get('fixtures/groups.json').success(function(data) {
+	for (group in data)
+	{
+            if (data[group].id == $scope.selectedGroup)
+		{
+		$scope.Group = data[group]
+		}
+    	}
+  });
 }]);
 
 g4lApp.controller('MainController', ['$scope', function ($scope) {
    $scope.currentPage = 'overview';
    $scope.goTo = function(to) {
     $scope.currentPage = to;
+   };
+   $scope.goToViewGroup = function(id) {
+    $scope.currentPage = 'viewGroup';
+    $scope.selectedGroup = id;	
    };
 }]);
 
